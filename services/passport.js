833774f2b577fc67fc92744c1fb4a282aadf246d
zgreathouse <greathouse.zachary@gmail.com>
2017-12-04 19:47:47 -0800

    modify google strategy in passport.js

--- services/passport.js
+++ services/passport.js
@@ -23,15 +23,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleID: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else{
-          new User({ googleID: profile.id }).save()
-            .then( user => done(null, user));
-        }
-      })
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id })
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      cost user = await new User({ googleId: profile.id}).save();
+      done(null, user)
     }
   )
 );
