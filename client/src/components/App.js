5fa0ba2526012d98c512ea4000b17f1c73a6ac8c
zgreathouse <greathouse.zachary@gmail.com>
2017-12-11 20:20:30 -0800

    add materializecss npm module, import materializecss in client/src/index.js, build out structure of header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//components
+import Header from './Header';
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return (
-    <div>
-      Got it memorized?
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
 
 import App from './components/App';
+import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware());
 
