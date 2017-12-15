43f9ce65446662be4ad1d334f7598ded29551bf8
zgreathouse <greathouse.zachary@gmail.com>
2017-12-15 15:01:54 -0800

    add body-parser npm mudule to parse payload for stripe tokens, refactor index.js file to import and use the body parser, add stripe npm module, create billingRoutes.js file, import keys and stripe, begin writing post request route for billing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 require('./models/User');
@@ -13,6 +14,7 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 2592000000,
@@ -23,6 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
