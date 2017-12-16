be85223c4589aea0a08b9cd823fb1566bad24be8
zgreathouse <greathouse.zachary@gmail.com>
2017-12-15 18:13:15 -0800

    fix bug in google strategy

--- services/passport.js
+++ services/passport.js
@@ -27,7 +27,7 @@ passport.use(
       const existingUser = await User.findOne({ googleId: profile.id });
 
       if (existingUser) {
-        done(null, existingUser);
+        return done(null, existingUser);
       }
 
       const user = await new User({ googleId: profile.id}).save();
