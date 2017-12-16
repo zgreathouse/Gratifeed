9c05b78de24d901307877d429317dbf64061a90e
zgreathouse <greathouse.zachary@gmail.com>
2017-12-15 17:41:24 -0800

    refactor server/index.js for production deployment to find index.html file for running react-app

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+//middlewares
 app.use(bodyParser.json());
 app.use(
   cookieSession({
@@ -24,8 +25,23 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+//routes
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  /* Express will serve up production assetts
+    like our main.js file, or main.css file */
+  app.use(express.static('client/build'));
+
+  /* Express will serve up the index.html file
+  if it doesn't recognize the route */
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+//Dynamic Port Binding
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
