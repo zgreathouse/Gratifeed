69e6fab21af21ce38d70f3c0ca7f49130ebd782d
zgreathouse <greathouse.zachary@gmail.com>
2017-12-22 12:22:33 -0800

    create new SurveyFormReview component, refactor SurveyNew component to toggle between SurveyForm and SurveyFormReview using a local state, modify SurveyForm handleSubmit callback to toggle the SurveyNew localState variable

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -3,12 +3,33 @@ import React, { Component} from 'react';
 
 //components
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+  state = { showFormReview: false };
+
+  renderContent() {
+    const { showFormReview } = this.state;
+
+    if (showFormReview) {
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showFormReview: false})}
+        />
+      );
+    }
+
+    return (
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true})}
+      />
+    );
+  }
+
   render() {
     return (
       <div>
-        <SurveyForm />
+        {this.renderContent()}
       </div>
     );
   }
