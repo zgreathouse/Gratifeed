a02f601109a3cbfe853a61065995d44ec888fd91
zgreathouse <greathouse.zachary@gmail.com>
2017-12-23 23:55:11 -0800

    add linkedin and github links to Landing component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -8,6 +8,20 @@ const Landing = () => {
         Gratifeed!
       </h1>
       Collect feedback from your users
+      <div className="bottom-right">
+        <button
+          href="https://www.linkedin.com/in/zachary-greathouse-11345813b/"
+          className="grey right white-text"
+        >
+          Linkedin
+        </button>
+        <button
+          href="https://github.com/zgreathouse/Gratifeed"
+          className="grey right white-text"
+        >
+          Github
+        </button>
+      </div>
     </div>
   );
 };
