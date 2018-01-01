77f03f6bad7a3a1b94256f8f3975a4ac5443b191
zgreathouse <greathouse.zachary@gmail.com>
2018-01-01 15:40:18 -0800

    add comments

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,6 +5,7 @@ import authReducer from './authReducer';
 import { reducer as reduxForm } from 'redux-form'; //a special reducer to handle the redux form slice of state
 import surveysReducer from './surveysReducer';
 
+//redux state shape
 export default combineReducers({
   auth: authReducer,
   form: reduxForm,
