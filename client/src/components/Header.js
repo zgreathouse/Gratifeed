9c8b417122c90aeaa6f050003aaf4deaf2a65f44
zgreathouse <greathouse.zachary@gmail.com>
2018-01-01 16:44:04 -0800

    add margin to Gratifeed logo in Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -37,6 +37,7 @@ class Header extends Component {
           <Link
             to={this.props.auth ? '/surveys' : '/'}
             className="left brand-logo"
+            style={{ marginLeft: '10px'}}
           >
             Gratifeed
           </Link>
